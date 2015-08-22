13bd7160a301436c46c2e687ec577ba4bd4a26d1
Daniel Huang <danielhuang@g.ucla.edu>
2015-08-21 20:27:31 -0700

    finished home!

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,14 @@
  */
 $( document ).ready(function() {
     $('.collapse').collapse();
+
+    $('#lol').on('mouseover',function(){
+        swal({   title: "{Coder.Central();",
+                text: "The monkeys behind the website! Visit www.codercentral.org for a website like this for only $500",
+                imageUrl: "js/lol.jpg"
+        });
+    });
+    $(function(){
+        $("ul#ticker02").liScroll({travelocity: 0.15});
+    });
 });
